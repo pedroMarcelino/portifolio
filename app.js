5f54b69e1e95d40def1b084ad5344111878237bf
pedroMarcelino <pedor.marcelinodv@gmail.com>
2024-07-14 19:53:29 -0300

    finalizado desing

--- app.js
+++ app.js
@@ -10,18 +10,6 @@ document.addEventListener('DOMContentLoaded', () =>{
     .go()
 })
 
-document.addEventListener('DOMContentLoaded', () =>{
-    new TypeIt('.animated2', {
-        speed: 200,
-        loop: true
-    }).type('Back-End', {delay: 900}).delete(8).pause(1500)
-        .type('Front-End', {delay: 900}).delete(9).pause(1500)
-        .type('Web', {delay: 900}).delete(3).pause(1500)
-        .type('Full-Stack', {delay: 900}).delete(10).pause(1500)
-    
-    .go()
-})
-
 document.addEventListener('click', function(event) {
     // Verificar se o elemento clicado é um <a> ou um <li> dentro da navbar
     if (event.target.matches('.nav-link') || event.target.closest('.nav-item')) {
